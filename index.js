0a216c26264e61414146b15c40bc645250d121b8
w3sl1y <SV73623892@idat.edu.pe>
2021-09-12 13:50:19 -0500

    Actualizado

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ function calcCircumfrence(radius) {
   //Calcular años perrunos
   function calculateDogAge(age) {
     var dogYears = 7*age;
-    console.log("Your doggie is " + dogYears + " years old in dog years!");
+    console.log("¡Tu perrito tiene " + dogYears + " años en años de perro!");
 }
