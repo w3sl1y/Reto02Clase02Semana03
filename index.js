040025bceb7abb1a143a615defba8cfb8fab1435
w3sl1y <SV73623892@idat.edu.pe>
2021-09-12 13:46:52 -0500

    Actualización

---
+++ index.js
@@ -0,0 +1,14 @@
+//Geometrizador
+function calcCircumfrence(radius) {
+    var y = (Math.PI * radius) * 2;
+  console.log("La circunferencia es " + y + ".");
+  }
+  function calcArea(radius) {
+    var x = (Math.PI * radius) * radius;
+  console.log("El área es " + x + ".");
+  }
+  //Calcular años perrunos
+  function calculateDogAge(age) {
+    var dogYears = 7*age;
+    console.log("Your doggie is " + dogYears + " years old in dog years!");
+}
